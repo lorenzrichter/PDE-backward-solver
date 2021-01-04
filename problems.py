ded2ef79b5a880007168182a2119881031fb71be
Leon <Leon@Leon>
2021-01-04 15:29:50 +0100

    added unboundedsin

--- problems.py
+++ problems.py
@@ -156,3 +156,63 @@ class AllenKahn():
     def v_true(self, x, t):
         print('no reference solution known')
         return 0
+
+
+class UnboundedSin():
+    def __init__(self, name='CosExp', d=1, T=1, seed=42, modus='np'):
+
+        np.random.seed(seed)
+        self.modus = modus
+        self.name = name
+        self.d = d
+        self.T = T
+        self.B = 1/np.sqrt(self.d)*np.eye(self.d)
+        self.B_pt = pt.tensor(self.B).float()
+        self.alpha = np.ones([self.d, 1]) # not needed, can delete?
+        self.alpha_pt = pt.tensor(self.alpha).float() # not needed, can delete?
+        self.X_0 = np.zeros(self.d)
+        self.rescal = 0.5
+        self.Sig = self.B[0,0]
+        self.delta_t_v = 0.001
+
+    def b(self, x):
+        if self.modus == 'pt':
+            return 0
+            # return pt.zeros(x.shape)
+        return 0
+        # return np.zeros(x.shape)
+
+    def sigma(self, x):
+        if self.modus == 'pt':
+            return pt.tensor(self.B_pt)
+        return self.B
+
+    def h( self, t, x, u, Du):
+        # u time derivative
+        Ut = - np.mean(np.where(x < 0,  np.sin(x),x), axis=-1)
+        # u value
+        xSum= x @ np.arange(1.,self.d+1.)
+        cosU= np.cos(xSum)
+        UVal = -(self.T-t) * Ut+ cosU
+        # U X derivarive (sum)
+        DUVal = (self.T-t)*np.mean(np.where(x < 0,  np.cos(x),np.ones(np.shape(x))), axis=-1) - self.d*(self.d+1.)/2.* np.sin(xSum)
+        # sum of diag of Hessian
+        D2UVal = -cosU* self.d*(self.d+1)*(2*self.d+1)/6. - (self.T-t)*np.mean(np.where(x < 0,  np.sin(x),np.zeros(np.shape(x))), axis=-1)
+       
+        # ret =  - Ut- 0.5*self.Sig*self.Sig*D2UVal - self.rescal*(UVal*DUVal/self.d + UVal*UVal)+ self.rescal*( np.power(u,2.) + np.multiply(u,np.mean(Du,axis=-1)))
+        ret =  - Ut- 0.5*self.Sig*self.Sig*D2UVal - self.rescal*(UVal*DUVal/self.d + UVal*UVal)+ self.rescal*( np.power(u,2.) + 1/np.sqrt(self.d) * np.multiply(u,np.sum(Du,axis=-1)))
+        return  ret.squeeze()
+
+    def g(self, x):
+        if self.modus == 'pt':
+            return 0
+        a = 1.0 * np.arange(1, self.d + 1)
+        return np.cos(x@a)
+
+    def u_true(self, x, t):
+        print('no reference solution known')
+        return 0
+    
+    def v_true(self, x, t):
+        a = 1.0 * np.arange(1, self.d + 1)        
+        return (self.T-t) * np.mean(np.where(x < 0,  np.sin(x),x), axis=-1) + np.cos(x@a)
